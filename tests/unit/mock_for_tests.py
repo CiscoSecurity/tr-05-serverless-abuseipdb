35d4171d8a2c1326d92d1c5e4ae261c495bcfd2f
GeorgiyII <gosha1104@gmail.com>
2020-03-25 13:05:22 +0200

    fixed tests

--- tests/unit/mock_for_tests.py
+++ tests/unit/mock_for_tests.py
@@ -98,7 +98,7 @@ EXPECTED_RESPONSE_OBSERVE = {
                     'short_description': 'description for id 15',
                     'title': 'title for id 15',
                     'type': 'indicator',
-                    'valid_time': []
+                    'valid_time': {}
                 },
                 {
                     'confidence': 'Medium',
@@ -114,7 +114,7 @@ EXPECTED_RESPONSE_OBSERVE = {
                     'short_description': 'description for id 21',
                     'title': 'title for id 21',
                     'type': 'indicator',
-                    'valid_time': []
+                    'valid_time': {}
                 },
                 {
                     'confidence': 'Medium',
@@ -130,20 +130,23 @@ EXPECTED_RESPONSE_OBSERVE = {
                     'short_description': 'description for id 16',
                     'title': 'title for id 16',
                     'type': 'indicator',
-                    'valid_time': []
+                    'valid_time': {}
                 },
                 {
                     'confidence': 'Medium',
                     'description': 'description for id 20',
                     'external_references': [
-                        {'source_name': 'AbuseIPDB',
-                         'url': 'https://www.abuseipdb.com/categories'}],
+                        {
+                            'source_name': 'AbuseIPDB',
+                            'url': 'https://www.abuseipdb.com/categories'
+                        }
+                    ],
                     'producer': 'AbuseIPDB',
                     'schema_version': '1.0.16',
                     'short_description': 'description for id 20',
                     'title': 'title for id 20',
                     'type': 'indicator',
-                    'valid_time': []
+                    'valid_time': {}
                 }
             ]
         },
