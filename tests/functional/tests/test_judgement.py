74892d4b46d6b7451be193d09254954c430562bb
GeorgiyII <gosha1104@gmail.com>
2020-05-20 12:32:59 +0300

    fixed functional tests

--- tests/functional/tests/test_judgement.py
+++ tests/functional/tests/test_judgement.py
@@ -23,7 +23,7 @@ def test_positive_judgement_ip_observable(module_headers):
         **{'headers': module_headers}
     )['data']
     judgements = get_observables(response, 'Abuse IPDB')['data']['judgements']
-    assert judgements['count'] == 25
+    assert judgements['count'] == 24
     for judgement in judgements['docs']:
         assert judgement['type'] == 'judgement'
         assert judgement['id']
