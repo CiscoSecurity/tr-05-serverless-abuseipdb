e79e276181892ac4e2b7a15a5a895493e5256e63
GeorgiyII <gosha1104@gmail.com>
2020-05-28 16:19:10 +0300

    added tests

--- tests/unit/api/test_enrich.py
+++ tests/unit/api/test_enrich.py
@@ -37,9 +37,6 @@ def abuse_api_request():
 def abuse_api_response(*, ok, status_error=None, payload=None):
     mock_response = mock.MagicMock()
 
-    if payload:
-        payload = payload
-
     mock_response.ok = ok
 
     if ok:
@@ -87,16 +84,21 @@ def valid_json():
 
 
 @fixture(scope='module')
-def expected_payload(route, client):
+def valid_json_multiple():
+    return [
+        {'type': 'ip', 'value': '118.25.6.39'},
+        {'type': 'ip', 'value': '127.0.0.1'}
+    ]
 
+
+@fixture(scope='module')
+def expected_payload(route, client):
     payload = None
 
     if route.startswith('/deliberate'):
-
         payload = EXPECTED_RESPONSE_DELIBERATE
 
     if route.startswith('/observe'):
-
         payload = EXPECTED_RESPONSE_OBSERVE
 
     return payload
@@ -105,7 +107,6 @@ def expected_payload(route, client):
 @mock.patch('api.enrich.get_categories_objects')
 def test_enrich_call_success(categories_mock, route, client, valid_jwt,
                              valid_json, abuse_api_request, expected_payload):
-
     categories_mock.return_value = ABUSE_CATEGORIES
     abuse_api_request.return_value = abuse_api_response(ok=True)
 
@@ -120,7 +121,6 @@ def test_enrich_call_success(categories_mock, route, client, valid_jwt,
     assert data['data']['verdicts']['docs'][0].pop('valid_time')
 
     if route == '/observe/observables':
-
         judgements = data['data']['judgements']
         assert judgements['count'] == 2
         assert judgements['docs'][0].pop('id')
@@ -156,10 +156,70 @@ def test_enrich_call_success(categories_mock, route, client, valid_jwt,
     assert data == expected_payload
 
 
+@mock.patch('api.enrich.get_categories')
+def test_enrich_call_error_with_data(categories_mock, route, client, valid_jwt,
+                                     valid_json_multiple, abuse_api_request,
+                                     expected_payload):
+    categories_mock.return_value = ABUSE_CATEGORIES
+    abuse_api_request.side_effect = (
+        abuse_api_response(ok=True),
+        abuse_api_response(ok=False, status_error=HTTPStatus.TOO_MANY_REQUESTS,
+                           payload=ABUSE_429_RESPONSE)
+    )
+
+    response = client.post(
+        route, headers=headers(valid_jwt), json=valid_json_multiple
+    )
+
+    assert response.status_code == HTTPStatus.OK
+
+    data = response.get_json()
+
+    assert data['data']['verdicts']['docs'][0].pop('valid_time')
+
+    if route == '/observe/observables':
+        judgements = data['data']['judgements']
+        assert judgements['count'] == 2
+        assert judgements['docs'][0].pop('id')
+        assert judgements['docs'][1].pop('id')
+
+        sightings = data['data']['sightings']
+        assert sightings['count'] == 2
+        sighting_id_1 = sightings['docs'][0].pop('id')
+        sighting_id_2 = sightings['docs'][1].pop('id')
+
+        indicators = data['data']['indicators']
+        assert indicators['count'] == 4
+        indicator_id_1 = indicators['docs'][0].pop('id')
+        indicator_id_2 = indicators['docs'][1].pop('id')
+        indicator_id_3 = indicators['docs'][2].pop('id')
+        indicator_id_4 = indicators['docs'][3].pop('id')
+
+        relationships = data['data']['relationships']
+        assert relationships['count'] == 4
+        assert relationships['docs'][0].pop('id')
+        assert relationships['docs'][1].pop('id')
+        assert relationships['docs'][2].pop('id')
+        assert relationships['docs'][3].pop('id')
+        assert relationships['docs'][0].pop('source_ref') == sighting_id_1
+        assert relationships['docs'][1].pop('source_ref') == sighting_id_1
+        assert relationships['docs'][2].pop('source_ref') == sighting_id_2
+        assert relationships['docs'][3].pop('source_ref') == sighting_id_2
+        assert relationships['docs'][0].pop('target_ref') == indicator_id_1
+        assert relationships['docs'][1].pop('target_ref') == indicator_id_2
+        assert relationships['docs'][2].pop('target_ref') == indicator_id_3
+        assert relationships['docs'][3].pop('target_ref') == indicator_id_4
+
+    expected_response = {}
+    expected_response.update(expected_payload)
+    expected_response.update(EXPECTED_RESPONSE_429_ERROR)
+
+    assert data == expected_response
+
+
 @mock.patch('api.enrich.get_categories_objects')
 def test_enrich_call_success_limit_1(categories_mock, route, client, valid_jwt,
                                      valid_json, abuse_api_request):
-
     categories_mock.return_value = ABUSE_CATEGORIES
     abuse_api_request.return_value = abuse_api_response(ok=True)
 
@@ -203,7 +263,6 @@ def test_enrich_call_success_limit_1(categories_mock, route, client, valid_jwt,
 
 def test_enrich_call_auth_error(route, client, valid_jwt, valid_json,
                                 abuse_api_request):
-
     abuse_api_request.return_value = abuse_api_response(
         ok=False, status_error=HTTPStatus.UNAUTHORIZED)
 
@@ -219,7 +278,6 @@ def test_enrich_call_auth_error(route, client, valid_jwt, valid_json,
 
 def test_enrich_call_404_error(route, client, valid_jwt, valid_json,
                                abuse_api_request):
-
     abuse_api_request.return_value = abuse_api_response(
         ok=False, status_error=HTTPStatus.NOT_FOUND)
 
@@ -235,7 +293,6 @@ def test_enrich_call_404_error(route, client, valid_jwt, valid_json,
 
 def test_enrich_call_500_error(route, client, valid_jwt, valid_json,
                                abuse_api_request):
-
     abuse_api_request.return_value = abuse_api_response(
         ok=False, status_error=HTTPStatus.INTERNAL_SERVER_ERROR)
 
@@ -249,9 +306,9 @@ def test_enrich_call_500_error(route, client, valid_jwt, valid_json,
     assert data == EXPECTED_RESPONSE_500_ERROR
 
 
-def test_enrich_call_422_error(route, client, valid_jwt, valid_json,
-                               abuse_api_request):
-
+@mock.patch('api.enrich.get_categories_objects')
+def test_enrich_call_422_error(categories_mock, route, client, valid_jwt,
+                               valid_json, abuse_api_request):
     abuse_api_request.return_value = abuse_api_response(
         ok=False, status_error=HTTPStatus.UNPROCESSABLE_ENTITY)
 
@@ -267,7 +324,6 @@ def test_enrich_call_422_error(route, client, valid_jwt, valid_json,
 
 def test_enrich_call_429_error(route, client, valid_jwt, valid_json,
                                abuse_api_request):
-
     abuse_api_request.return_value = abuse_api_response(
         ok=False,
         status_error=HTTPStatus.TOO_MANY_REQUESTS,
